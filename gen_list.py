0fa59ad866ece4efc4323feb7e8e48bf8bb76237
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-10-19 13:06:58 +0800

    feat: script

---
+++ gen_list.py
@@ -0,0 +1,30 @@
+import csv
+import json
+
+i18nTable = {}
+
+with open("ChineseSimplified.csv", "r", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+    fieldnames = reader.fieldnames
+    print(fieldnames)
+    for row in reader:
+        i18nTable[row["key"]] = row["value"]
+
+
+def getI18n(key):
+    return i18nTable.get(key, key)
+
+
+with open("ItemAssetsCollection.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+    entries = data["m_Structure"]["entries"]
+
+plainList = ""
+
+for i in entries:
+    metaData = i["metaData"]
+    line = f"{getI18n(metaData['displayName'])} ({metaData['id']}): {getI18n(metaData['description'])}\n"
+    plainList += line
+
+with open("list.txt", "w", encoding="utf-8") as f:
+    f.write(plainList)
